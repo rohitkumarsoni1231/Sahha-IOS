13c2ef5b50dc80515903c35ba45f29a6ff1dd101
Rohit Kumar <oasisrohit470@gmail.com>
2024-08-08 11:03:15 +0400

    Initial Commit

---
+++ Sahha-IOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sahha-IOS
+//
+//  Created by Rohit Kumar on 08/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sahha-IOS/Sahha_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sahha_IOSApp.swift
+//  Sahha-IOS
+//
+//  Created by Rohit Kumar on 08/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Sahha_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
